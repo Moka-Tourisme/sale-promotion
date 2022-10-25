51988614cfe5d9747cd20de8e34106f1200aa5b7
Romain <73525560+Rom10811@users.noreply.github.com>
2022-10-25 15:35:01 +0200

    [ADD] Gift card customization

---
+++ pos_gift_card_custom/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Point Of Sale Gift Card Custom',
+    'version': '1',
+    'category': 'Custom',
+    'summary': 'Point Of Sale Gift Card Custom',
+    'description': "Point Of Sale Gift Card Custom",
+    'depends': ['gift_card_custom', 'point_of_sale'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
---
+++ sale_gift_card_custom/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Gift Card Custom',
+    'version': '1',
+    'category': 'Custom',
+    'summary': 'Sale Gift card custom',
+    'description': "Sale Gift card custom",
+    'depends': ['gift_card_custom', 'sale'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
