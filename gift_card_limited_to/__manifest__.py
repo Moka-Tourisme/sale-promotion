bcd44c5eda07c3917973f590511f3ad2ba295c42
Damien <ultrarushgame@gmail.com>
2024-03-20 17:42:32 +0100

    [UPD] gift_card_limited_to fix

--- gift_card_limited_to/__manifest__.py
+++ gift_card_limited_to/__manifest__.py
@@ -7,7 +7,7 @@
     'category': 'Sales',
     "license": "AGPL-3",
     'summary': 'Module de personnalisation des cartes cadeaux',
-    'depends': ['gift_card', 'point_of_sale', 'sale', 'sale_gift_card'],
+    'depends': ['gift_card_design', 'pos_gift_card_design', 'sale_gift_card_design'],
     'data': [
         'views/gift_card_view.xml',
         'views/gift_card_design_templates.xml'
